4be5e2ad2d306478cd92cd250f29431a189cb44b
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-11-24 15:24:18 -0300

    Primeiro commit

---
+++ src/main/java/br/com/merx/model/Categoria.java
@@ -0,0 +1,70 @@
+package br.com.merx.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id_categoria;
+	
+	
+	private String categoria;
+
+
+	public Integer getId_categoria() {
+		return id_categoria;
+	}
+
+
+	public void setId_categoria(Integer id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_categoria == null) ? 0 : id_categoria.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id_categoria == null) {
+			if (other.id_categoria != null)
+				return false;
+		} else if (!id_categoria.equals(other.id_categoria))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
